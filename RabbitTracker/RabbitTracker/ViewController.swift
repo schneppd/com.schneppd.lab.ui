c54f5a77c62e318a88bb9383428e66c97ae118fd
cdsm <cdsm@macpf01.local>
2017-04-04 14:55:17 +0200

    add delegate + simple ui events

--- RabbitTracker/RabbitTracker/ViewController.swift
+++ RabbitTracker/RabbitTracker/ViewController.swift
@@ -8,17 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    //MARK: Properties
+    @IBOutlet weak var rabitInput: UITextField!
+    
+    @IBOutlet weak var rabbitLabel: UILabel!
 
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // connect to rabitInput to manage text value changes
+        rabitInput.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //MARK: Actions
+    @IBAction func ExecuteSetPrefferedRabbitBreed(_ sender: UIButton) {
+        rabbitLabel.text = "Default breed"
+    }
+    
+    //MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+            rabbitLabel.text = textField.text
+    }
+    
+
+    
+
 
 
 }
